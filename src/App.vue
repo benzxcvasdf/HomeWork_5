9896c5c1091bfd7b6d0c2daaab7ce4a727e0840f
benzxcvasdf <71902795+benzxcvasdf@users.noreply.github.com>
2023-05-27 20:07:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Logo from './components/Logo.vue'
+import Middle from "@/components/middle.vue";
+import Bottom from "@/components/bottom.vue";
+</script>
+
+<template>
+  <div style="position: relative;">
+    <div id="centerContent" class="wide125" style="opacity: 1; padding-top: 32px; position: absolute; top: 50%; left: 50%; transform: translate(-50%,0%);">
+      <Logo/>
+      <Middle/>
+      <Bottom/>
+    </div>
+
+
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+:root{
+  --bg-color:#323437;
+  --main-color:#e2b714;
+  --caret-color:#e2b714;
+  --sub-color:#646669;
+  --sub-alt-color:#2c2e31;
+  --text-color:#d1d0c5;
+  --error-color:#ca4754;
+  --error-extra-color:#7e2a33;
+  --colorful-error-color:#ca4754;
+  --colorful-error-extra-color:#7e2a33
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,473 @@
+<template>
+  <div id="top" class="">
+    <div class="logo">
+      <div class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="-680 -1030 300 180">
+          <g>
+            <path d="M -430 -910 L -430 -910 C -424.481 -910 -420 -905.519 -420 -900 L -420 -900 C -420 -894.481 -424.481 -890 -430 -890 L -430 -890 C -435.519 -890 -440 -894.481 -440 -900 L -440 -900 C -440 -905.519 -435.519 -910 -430 -910 Z"></path>
+            <path d=" M -570 -910 L -510 -910 C -504.481 -910 -500 -905.519 -500 -900 L -500 -900 C -500 -894.481 -504.481 -890 -510 -890 L -570 -890 C -575.519 -890 -580 -894.481 -580 -900 L -580 -900 C -580 -905.519 -575.519 -910 -570 -910 Z "></path>
+            <path d="M -590 -970 L -590 -970 C -584.481 -970 -580 -965.519 -580 -960 L -580 -940 C -580 -934.481 -584.481 -930 -590 -930 L -590 -930 C -595.519 -930 -600 -934.481 -600 -940 L -600 -960 C -600 -965.519 -595.519 -970 -590 -970 Z"></path>
+            <path d=" M -639.991 -960.515 C -639.72 -976.836 -626.385 -990 -610 -990 L -610 -990 C -602.32 -990 -595.31 -987.108 -590 -982.355 C -584.69 -987.108 -577.68 -990 -570 -990 L -570 -990 C -553.615 -990 -540.28 -976.836 -540.009 -960.515 C -540.001 -960.345 -540 -960.172 -540 -960 L -540 -960 L -540 -940 C -540 -934.481 -544.481 -930 -550 -930 L -550 -930 C -555.519 -930 -560 -934.481 -560 -940 L -560 -960 L -560 -960 C -560 -965.519 -564.481 -970 -570 -970 C -575.519 -970 -580 -965.519 -580 -960 L -580 -960 L -580 -960 L -580 -940 C -580 -934.481 -584.481 -930 -590 -930 L -590 -930 C -595.519 -930 -600 -934.481 -600 -940 L -600 -960 L -600 -960 L -600 -960 L -600 -960 L -600 -960 L -600 -960 L -600 -960 L -600 -960 C -600 -965.519 -604.481 -970 -610 -970 C -615.519 -970 -620 -965.519 -620 -960 L -620 -960 L -620 -940 C -620 -934.481 -624.481 -930 -630 -930 L -630 -930 C -635.519 -930 -640 -934.481 -640 -940 L -640 -960 L -640 -960 C -640 -960.172 -639.996 -960.344 -639.991 -960.515 Z "></path>
+            <path d=" M -460 -930 L -460 -900 C -460 -894.481 -464.481 -890 -470 -890 L -470 -890 C -475.519 -890 -480 -894.481 -480 -900 L -480 -930 L -508.82 -930 C -514.99 -930 -520 -934.481 -520 -940 L -520 -940 C -520 -945.519 -514.99 -950 -508.82 -950 L -431.18 -950 C -425.01 -950 -420 -945.519 -420 -940 L -420 -940 C -420 -934.481 -425.01 -930 -431.18 -930 L -460 -930 Z "></path>
+            <path d="M -470 -990 L -430 -990 C -424.481 -990 -420 -985.519 -420 -980 L -420 -980 C -420 -974.481 -424.481 -970 -430 -970 L -470 -970 C -475.519 -970 -480 -974.481 -480 -980 L -480 -980 C -480 -985.519 -475.519 -990 -470 -990 Z"></path>
+            <path d=" M -630 -910 L -610 -910 C -604.481 -910 -600 -905.519 -600 -900 L -600 -900 C -600 -894.481 -604.481 -890 -610 -890 L -630 -890 C -635.519 -890 -640 -894.481 -640 -900 L -640 -900 C -640 -905.519 -635.519 -910 -630 -910 Z "></path>
+            <path d=" M -515 -990 L -510 -990 C -504.481 -990 -500 -985.519 -500 -980 L -500 -980 C -500 -974.481 -504.481 -970 -510 -970 L -515 -970 C -520.519 -970 -525 -974.481 -525 -980 L -525 -980 C -525 -985.519 -520.519 -990 -515 -990 Z "></path>
+            <path d=" M -660 -910 L -680 -910 L -680 -980 C -680 -1007.596 -657.596 -1030 -630 -1030 L -430 -1030 C -402.404 -1030 -380 -1007.596 -380 -980 L -380 -900 C -380 -872.404 -402.404 -850 -430 -850 L -630 -850 C -657.596 -850 -680 -872.404 -680 -900 L -680 -920 L -660 -920 L -660 -900 C -660 -883.443 -646.557 -870 -630 -870 L -430 -870 C -413.443 -870 -400 -883.443 -400 -900 L -400 -980 C -400 -996.557 -413.443 -1010 -430 -1010 L -630 -1010 C -646.557 -1010 -660 -996.557 -660 -980 L -660 -910 Z "></path>
+          </g>
+        </svg>
+      </div>
+      <h1 class="text">
+        monkeytype
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo"
+}
+</script>
+
+<style scoped>
+
+:root {
+  --bg-color:#323437;
+  --main-color:#e2b714;
+  --caret-color:#e2b714;
+  --sub-color:#646669;
+  --sub-alt-color:#2c2e31;
+  --text-color:#d1d0c5;
+  --error-color:#ca4754;
+  --error-extra-color:#7e2a33;
+  --colorful-error-color:#ca4754;
+  --colorful-error-extra-color:#7e2a33
+  --roundness: 0.5rem;
+  --font: "Roboto Mono", "Vazirmatn", sans-serif;
+  font-weight: 400;
+  scroll-padding-top: 2rem;
+}
+::placeholder {
+  color: var(--sub-color);
+  opacity: 1;
+}
+::selection {
+  background-color: var(--main-color);
+  color: var(--sub-alt-color);
+}
+html {
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+}
+a {
+  color: var(--sub-color);
+  display: inline-block;
+  transition: 0.125s;
+}
+a:hover {
+  color: var(--text-color);
+}
+a[router-link] * {
+  pointer-events: none;
+}
+a.button {
+  text-decoration: none;
+}
+body {
+  background: var(--bg-color);
+  color: var(--text-color);
+  font-family: var(--font);
+  margin: 0;
+  min-height: 100vh;
+  overflow-x: hidden;
+  padding: 0;
+}
+.customBackground {
+  align-items: center;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  content: "";
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: -999;
+}
+#backgroundLoader {
+  animation: loader 2s cubic-bezier(0.38, 0.16, 0.57, 0.82) infinite;
+  background: var(--main-color);
+  height: 3px;
+  position: fixed;
+  width: 100%;
+  z-index: 9999;
+}
+#centerContent {
+  align-items: center;
+  display: grid;
+  gap: 2rem;
+  grid-auto-flow: row;
+  grid-template-rows: auto 1fr auto;
+  max-width: 1000px;
+  min-height: 100vh;
+  padding: 2rem;
+  transition: padding-top 0.125s;
+  width: 100%;
+  z-index: 999;
+}
+#centerContent.wide125 {
+  max-width: 1250px;
+}
+#centerContent.wide150 {
+  max-width: 1500px;
+}
+#centerContent.wide200 {
+  max-width: 2000px;
+}
+#centerContent.widemax {
+  max-width: unset;
+}
+
+* {
+  box-sizing: border-box;
+}
+.hidden {
+  display: none !important;
+}
+.invisible {
+  opacity: 0 !important;
+  pointer-events: none !important;
+}
+.button {
+  align-content: center;
+  appearance: none;
+  background: var(--sub-alt-color);
+  border: none;
+  border-radius: var(--roundness);
+  color: var(--text-color);
+  cursor: pointer;
+  font-family: inherit;
+  font-size: 1em;
+  height: min-content;
+  line-height: 1.25em;
+  padding: 0.5em;
+  text-align: center;
+  transition: background 0.125s, color 0.125s;
+  user-select: none;
+  -webkit-user-select: none;
+}
+.button.active {
+  background: var(--main-color);
+  color: var(--bg-color);
+}
+.button.active:hover {
+  background: var(--text-color);
+}
+.button.active:active {
+  background: var(--sub-color);
+  color: var(--bg-color);
+}
+.button.disabled {
+  cursor: default;
+  opacity: 0.33;
+  pointer-events: none;
+}
+.button.disabled:hover {
+  background: var(--text-color);
+  outline: none;
+}
+.button.disabled:active {
+  background: var(--sub-color);
+  color: var(--bg-color);
+}
+.button:hover {
+  background: var(--text-color);
+  color: var(--bg-color);
+  outline: none;
+}
+.button:focus {
+  box-shadow: 0 0 0 0.15em var(--text-color);
+}
+.button:active {
+  background: var(--sub-color);
+  color: var(--bg-color);
+}
+
+#bottom {
+  color: var(--sub-color);
+  font-size: 0.75rem;
+  line-height: 1rem;
+  padding: 0 5px;
+  position: relative;
+  text-align: center;
+}
+#bottom .keyTips {
+  line-height: 1.25rem;
+  margin-bottom: 2rem;
+  transition: 0.125s;
+}
+#bottom .left .textButton,
+#bottom .right .textButton {
+  transition: 0.125s;
+}
+#bottom #supportMeButton:hover {
+  color: var(--text-color);
+  cursor: pointer;
+}
+#bottom #commandLineMobileButton {
+  background: var(--main-color);
+  border-radius: 99rem;
+  color: var(--bg-color);
+  cursor: pointer;
+  display: none;
+  font-size: 1rem;
+  height: 3rem;
+  left: 0;
+  line-height: 3rem;
+  position: absolute;
+  text-align: center;
+  top: -4rem;
+  transition: 0.25s;
+  width: 3rem;
+  z-index: 99;
+}
+#bottom .leftright {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: auto auto;
+  user-select: none;
+}
+#bottom .leftright .textButton {
+  font-size: 0.75rem;
+  line-height: 1rem;
+  padding: 0;
+  text-decoration: none;
+}
+#bottom .leftright .left {
+  text-align: left;
+}
+#bottom .leftright .left,
+#bottom .leftright .right {
+  display: grid;
+  gap: 1rem;
+  grid-auto-flow: column;
+  width: fit-content;
+  width: -moz-fit-content;
+}
+#bottom .leftright .right {
+  justify-self: right;
+  text-align: right;
+}
+#bottom .leftright .left .textButton,
+#bottom .leftright .right .textButton {
+  align-items: baseline;
+  display: grid;
+  gap: 0.25rem;
+  grid-auto-flow: column;
+  width: max-content;
+  width: -moz-available;
+}
+#bottom #versionGroup {
+  display: flex;
+}
+#bottom #versionGroup #newVersionIndicator {
+  background-color: var(--main-color);
+  border-radius: calc(var(--roundness) / 2);
+  color: var(--bg-color);
+  cursor: pointer;
+  text-align: center;
+  transition: 0.125s;
+}
+#bottom.focus #newVersionIndicator,
+#bottom.focus .keyTips,
+#bottom.focus .textButton {
+  opacity: 0 !important;
+}
+#bottom.focus #commandLineMobileButton {
+  opacity: 0 !important;
+  pointer-events: none !important;
+}
+input,
+textarea {
+  background: var(--sub-alt-color);
+  border: none;
+  border-radius: var(--roundness);
+  caret-color: var(--main-color);
+  color: var(--text-color);
+  font-family: var(--font);
+  font-size: 1em;
+  line-height: 1.25em;
+  outline: none;
+  padding: 0.5em;
+}
+
+.pageLogin {
+  align-items: center;
+  display: flex;
+  gap: 1rem;
+  grid-auto-flow: column;
+  height: 100%;
+  justify-content: space-around;
+}
+.pageLogin .side {
+  display: grid;
+  gap: 0.5rem;
+  grid-template-columns: 1fr;
+  justify-content: center;
+}
+.pageLogin .side input {
+  width: 15rem;
+}
+.pageLogin .side.login {
+  grid-template-areas: "title forgotButton" "form form";
+}
+.pageLogin .side.login .title {
+  grid-area: title;
+}
+.pageLogin .side.login #forgotPasswordButton {
+  font-size: 0.75rem;
+  grid-area: forgotButton;
+  line-height: 0.75rem;
+}
+.pageLogin .side.login form {
+  grid-area: form;
+}
+.pageLogin form {
+  display: grid;
+  gap: 0.5rem;
+  width: 100%;
+}
+.pageLogin .preloader {
+  color: var(--main-color);
+  font-size: 2rem;
+  left: 50%;
+  position: fixed;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  transition: 0.25s;
+}
+#monkey {
+  animation: shake 0s infinite;
+  height: 0;
+  margin: 0 auto;
+  width: 308px;
+}
+#monkey div {
+  height: 200px;
+  position: fixed;
+  width: 308px;
+}
+#top {
+  align-items: center;
+  font-size: 2.3rem;
+  grid-auto-flow: column;
+  grid-template-areas: "logo menu";
+  line-height: 2.3rem;
+  padding: 0 5px;
+  z-index: 2;
+}
+#top .logo {
+  display: grid;
+  gap: 0.5rem;
+  grid-template-columns: auto 1fr;
+  user-select: none;
+}
+#top .logo {
+  cursor: pointer;
+  white-space: nowrap;
+}
+#top .logo .icon {
+  align-items: center;
+  background-color: transparent;
+  display: grid;
+  width: 2.5rem;
+}
+#top .logo .icon svg path {
+  fill: var(--main-color);
+  transition: 0.25s;
+}
+#top .logo .text {
+  font-size: 2rem;
+  font-weight: unset;
+  margin-block-end: unset;
+  margin-block-start: unset;
+  margin-top: -0.2em;
+  position: relative;
+  transition: color 0.25s;
+}
+#top .logo .text .top {
+  color: var(--sub-color);
+  font-size: 0.65rem;
+  left: 0.25rem;
+  line-height: 0.65rem;
+  position: absolute;
+  top: -0.1rem;
+  transition: color 0.25s, opacity 0.25s;
+}
+#top .logo .bottom {
+  color: var(--main-color);
+  cursor: pointer;
+  margin-left: -0.15rem;
+  transition: 0.25s;
+}
+#top .config {
+  grid-area: config;
+  transition: 0.125s;
+}
+#top .config .mobileConfig {
+  display: none;
+}
+#top .config .mobileConfig .textButton {
+  font-size: 2rem;
+  margin-right: -1rem;
+  padding: 0.5rem 1rem;
+}
+#top .config .desktopConfig {
+  grid-gap: 0.2rem;
+  display: grid;
+  grid-template-rows: 0.7rem 0.7rem 0.7rem;
+  justify-items: self-end;
+  justify-self: right;
+}
+#top .config .desktopConfig .group .title {
+  color: var(--sub-color);
+  font-size: 0.5rem;
+  line-height: 0.5rem;
+  margin-bottom: 0.15rem;
+}
+#top .config .desktopConfig .group .buttons {
+  display: flex;
+}
+#top .config .desktopConfig .group .buttons .textButton {
+  font-size: 0.7rem;
+  line-height: 0.7rem;
+  padding: 0 0.15rem;
+}
+#top .config .desktopConfig .group.disabled {
+  opacity: 0.25;
+  pointer-events: none;
+}
+#top .config .desktopConfig .numbersMode,
+#top .config .desktopConfig .punctuationMode {
+  margin-bottom: -0.1rem;
+}
+#top.focus {
+  color: var(--sub-color) !important;
+}
+#top.focus .result {
+  opacity: 0 !important;
+}
+#top.focus .icon svg path {
+  fill: var(--sub-color) !important;
+}
+#top.focus .logo .text {
+  color: var(--sub-color) !important;
+}
+#top.focus .config,
+#top.focus .logo .top {
+  opacity: 0 !important;
+}
+
+</style>
